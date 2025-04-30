287126d0e9bef18e9bd6f7758c365a54363fadf9
F33RNI <fernlane@proton.me>
2025-04-29 19:15:56 -0500

    refactor: update copyright date

--- recorder.py
+++ recorder.py
@@ -1,5 +1,5 @@
 """
-Copyright (C) 2023-2024 Fern Lane, Homeland-sees automated surveillance camera project
+Copyright (C) 2023-2025 Fern Lane, Homeland-sees automated surveillance camera project
 
 Licensed under the GNU Affero General Public License, Version 3.0 (the "License");
 you may not use this file except in compliance with the License.
